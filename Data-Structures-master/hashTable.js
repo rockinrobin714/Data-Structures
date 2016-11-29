a6cb8be4ff35fb50b3672020fe1e38351de0220c
Robin Dykema <robindykema@gmail.com>
2016-11-29 14:28:38 -0600

    Update and rename hash-table to hashTable.js

--- Data-Structures-master/hashTable.js
+++ Data-Structures-master/hashTable.js
@@ -23,27 +23,35 @@ var makeHashTable = function() {
      }
     },
     delete: function(key) {
+    //Just like retrieve, I use the hash function to get the bucket index
       var index = hashFn(key,max);
       var bucket = this._storage[index];
+      //Once you figure out which bucket to use, loop through the bucket's arrays to find the thing you are deleting
       for (var i=0;i<bucket.length;i++){
         if (bucket[i][0]===key){
+        //Delete the key from the bucket
         bucket.splice(i,1);
         }
       }
     },
     insert: function(key, value) {
       var index = hashFn(key,max);
+      //If the bucket does not exist yet, create it.
       if(!this._storage[index]){
         this._storage[index] = [];
       }
+      //Now that it exists, I created a found variable so when I loop through the bucket, 
+      //I can see if something exists with that key name and update it
       var bucket = this._storage[index];
       var found = false;
       for (var i=0;i<bucket.length;i++){
         if (bucket[i][0]===key){
+        //If it is found, update it with the new value
           bucket[i][1]=value;
           found=true;
         }
     } if (!found){
+    //If the key doesn't exist in the bucket, add it
       bucket.push([key,value]);
     }
   }
